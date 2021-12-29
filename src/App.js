296d262a785ec823c2b23033af1a60c305ff8abc
NituX <78910113+NituX@users.noreply.github.com>
2021-12-29 09:49:24 +0200

    Added React-router-dom
    Added react-router-dom and linked pages and buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu.json';
 import Header from './components/Header.js';
 import Login from './components/Login.js';
 import Register from './components/Register.js';
 import Restaurant from './components/Restaurant.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
 
   const [items] = useState(Menu.items);
 
   return (
-    <div className="App">
-      <Header/>
-      <Restaurant
-      items = {items}/>
-    </div>
-
+    <BrowserRouter>
+      <div className="App">
+      <Header />
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/restaurant" element={ <Restaurant items={items} /> } />
+          <Route path="/login" element={ <Login/> } />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     return (
-        <div className= {styles.container}>
+        <div className={styles.container}>
             <body>
                 LOGIN
             </body>
-            <div className= {styles.inputfield}>
+            <div className={styles.inputfield}>
                 <body>Email</body>
-                <input type='text'/>
+                <input type='text' />
             </div>
-            <div className= {styles.inputfield}>
+            
+            <div className={styles.inputfield}>
                 <body>Password</body>
-                <input type='text'/>
+                <input type='text' />
             </div>
 
-            <button> Create Account </button>
-
+            <Link to="/register">
+                <button> Create Account </button>
+            </Link>
         </div>
     )
 }
